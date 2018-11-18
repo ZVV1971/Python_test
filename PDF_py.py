eef3aa18a0e64adcfdc94576fcbc55d914484938
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2018-11-18 16:41:25 +0300

    None

--- PDF_py.py
+++ PDF_py.py
@@ -1,26 +1,40 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import io
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics 
+from reportlab.pdfbase.ttfonts import TTFont 
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import A4
+
+#Set the Arial font
 
 packet = io.BytesIO()
 
-# create a new PDF with Reportlab
-can = canvas.Canvas(packet, pagesize=letter)
-can.drawString(100, 100, "Hello world")
-can.save()
+pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
+
+canvas = Canvas(packet, pagesize=A4)
+canvas.setFont('Arial', 13)
+canvas.rotate(90)
+canvas.drawString(20, -A4[0]+35, "Копия верна")
+canvas.drawString(20, -A4[0]+50, "В.В. Захаренков")
+canvas.drawString(20, -A4[0]+65, '')
+canvas.drawString(20, -A4[0]+80, "Специалист по продажам")
+#canvas.showPage()
+
+canvas.save()
 
 # move to the beginning of the StringIO buffer
 packet.seek(0)
 new_pdf = PdfFileReader(packet)
 # read your existing PDF
-existing_pdf = PdfFileReader(open("mypdf.pdf", "rb"))
+existing_pdf = PdfFileReader(open("81801892.pdf", "rb"))
 output = PdfFileWriter()
 # add the "watermark" (which is the new pdf) on the existing page
-page = existing_pdf.getPage(0)
-page2 = new_pdf.getPage(0)
-page.mergePage(page2)
-output.addPage(page)
+for i in range(existing_pdf.getNumPages()):
+    page = existing_pdf.getPage(i)
+    page2 = new_pdf.getPage(0)
+    page.mergePage(page2)
+    output.addPage(page)
 # finally, write "output" to a real file
 outputStream = open("newpdf.pdf", "wb")
 output.write(outputStream)
