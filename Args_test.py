1032a7c5d1fa5f83a49aff1548c1f2af4c24f7b6
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2018-11-19 20:12:53 +0300

    Args merged with PDF processing

--- Args_test.py
+++ Args_test.py
@@ -4,15 +4,21 @@ import argparse
 parser = argparse.ArgumentParser(description='Gets some strings and prints'
                                  'either their values or defaults.')
 parser.add_argument('-p', dest = 'Position',
-                    help = 'Position of the sigining person',
+                    help = 'Position of the signing person',
                     default = 'Директор', type = str,
                     required = False)
 parser.add_argument('-n', dest = 'Name',
                     default = 'А.В. Кимленко', type = str,
-                    help = 'Family Name and initials of the sigining person',
+                    help = 'Family Name and initials of the signing person',
                     required = False)
+parser.add_argument('-f', dest = 'FileNames',
+                    help = 'PDF files to be processed',
+                    nargs='+',
+                    required = True)
 
 #this line is necessary to process arguments though
 #it should be done in an automatic mode
 args = parser.parse_args()
-#print(args['Position'])
+for i in args.FileNames:
+    print(i)
+print(args.Position)
