2a629af2ec628498473ec390962ec242ae8889a6
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2018-11-22 21:58:02 +0300

    ToolTip attempt

--- GUI/extract_from_TTN.py
+++ GUI/extract_from_TTN.py
@@ -3,6 +3,34 @@ import tkinter.filedialog as Dialog
 
 root = Tk()
 
+class CreateToolTip(object):
+    '''
+    create a tooltip for a given widget
+    '''
+    def __init__(self, widget, text='widget info'):
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.close)
+    def enter(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + 20
+        # creates a toplevel window
+        self.tw = tk.Toplevel(self.widget)
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(self.tw, text=self.text, justify='left',
+                       background='yellow', relief='solid', borderwidth=1,
+                       font=("times", "8", "normal"))
+        label.pack(ipadx=1)
+    def close(self, event=None):
+        if self.tw:
+            self.tw.destroy()
+
+
 class MyFirstGUI:
         
     def __init__(self, master):
@@ -12,18 +40,19 @@ class MyFirstGUI:
         menu = Menu(master, tearoff=0)
         menu2 = Menu(master, tearoff=0)
 
-        menubar=Menu(menu, tearoff=0)
+        self.menubar=Menu(menu, tearoff=0)
         menu.add_command(label="Open", command=self.open)
         menu.add_separator()
-        menu.add_command(label="Exit", command=master.destroy)
-
-        menubar.add_cascade(label="File", menu=menu)
+        mm=menu.add_command(label="Exit", command=master.destroy)
+        
+        self.menubar.add_cascade(label="File", menu=menu, state="normal")
 
         menu2.add_command(label="Copies")
         
-        self.m = menubar.add_cascade(label="Operations", menu=menu2)
+        self.menubar.add_cascade(label="Operations", menu=menu2,
+                                     state="disabled")
 
-        master.config(menu=menubar)
+        master.config(menu=self.menubar)
 
     def open(self):
         print("Opening files")
@@ -36,7 +65,7 @@ class MyFirstGUI:
                                           )
         for nm in filename:
             print(nm)
-
+        self.menubar.entryconfig("Operations", state="normal")
 
 my_gui = MyFirstGUI(root)
 root.mainloop()
