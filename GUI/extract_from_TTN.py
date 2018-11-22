859e8e2adbfb875bd6494e77f1c560bf6ab1808b
ZVV1971 <vvzakharenkov@mail.ru>
2018-11-22 15:44:48 +0300

    GUI updated

--- GUI/extract_from_TTN.py
+++ GUI/extract_from_TTN.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Label, Button, Menu, Menubutton
+import tkinter.filedialog as Dialog
 
 root = Tk()
 
@@ -8,10 +9,26 @@ class MyFirstGUI:
         self.master = master
         master.title("Сертификаты из ТТН")
         master.geometry("300x100")
-        main_menu = Menu(master)
+        menu = Menu(master, tearoff=0)
 
-    def hello(self):
-        pass
+        menubar=Menu(menu, tearoff=0)
+        menu.add_command(label="Open", command=self.open)
+        menu.add_separator()
+        menu.add_command(label="Exit", command=master.destroy)
+
+        menubar.add_cascade(label="Test", menu=menu)
+        
+        master.config(menu=menubar)
+
+    def open(self):
+        print("Opening files")
+        filename = Dialog.askopenfilename(initialdir = "/",
+                                          title="Файлы с ТТН",
+                                          #neede to leave comma to build 1-x tuple
+                                          filetypes = (("Excel files","*.xls"),),
+                                          multiple=True
+                                          )
+        print(filename)
 
 my_gui = MyFirstGUI(root)
 root.mainloop()
