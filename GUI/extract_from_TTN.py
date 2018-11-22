d96ec7a9216d011b7d244a14f73dc3077b417791
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2018-11-22 20:23:07 +0300

    menu added

--- GUI/extract_from_TTN.py
+++ GUI/extract_from_TTN.py
@@ -10,25 +10,33 @@ class MyFirstGUI:
         master.title("Сертификаты из ТТН")
         master.geometry("300x100")
         menu = Menu(master, tearoff=0)
+        menu2 = Menu(master, tearoff=0)
 
         menubar=Menu(menu, tearoff=0)
         menu.add_command(label="Open", command=self.open)
         menu.add_separator()
         menu.add_command(label="Exit", command=master.destroy)
 
-        menubar.add_cascade(label="Test", menu=menu)
+        menubar.add_cascade(label="File", menu=menu)
+
+        menu2.add_command(label="Copies")
         
+        self.m = menubar.add_cascade(label="Operations", menu=menu2)
+
         master.config(menu=menubar)
 
     def open(self):
         print("Opening files")
+
         filename = Dialog.askopenfilename(initialdir = "/",
                                           title="Файлы с ТТН",
-                                          #neede to leave comma to build 1-x tuple
+                                          #need to leave comma to build 1-x tuple
                                           filetypes = (("Excel files","*.xls"),),
                                           multiple=True
                                           )
-        print(filename)
+        for nm in filename:
+            print(nm)
+
 
 my_gui = MyFirstGUI(root)
 root.mainloop()
