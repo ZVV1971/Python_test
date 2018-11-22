788e3e24ae53909b5eded5f4255a1c7d6e9dc341
ZVV1971 <vvzakharenkov@mail.ru>
2018-11-22 10:13:24 +0300

    GUI

--- GUI/extract_from_TTN.py
+++ GUI/extract_from_TTN.py
@@ -1,1 +1,18 @@
- 
+from tkinter import Tk, Label, Button, Menu, Menubutton
+
+root = Tk()
+
+class MyFirstGUI:
+        
+    def __init__(self, master):
+        self.master = master
+        master.title("Сертификаты из ТТН")
+        master.geometry("300x100")
+        main_menu = Menu(master)
+
+    def hello(self):
+        pass
+
+my_gui = MyFirstGUI(root)
+root.mainloop()
+
